cf60c53ed4b5dd8a15d975748c4375f2b1aa0969
stha9840 <aayushstha610@gmail.com>
2025-05-31 19:11:59 +0545

    Vehicle category option added

--- lib/bottom_screen/home.dart
+++ lib/bottom_screen/home.dart
@@ -6,7 +6,6 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade100,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10),
@@ -51,7 +50,7 @@ class Home extends StatelessWidget {
                   prefixIcon: const Icon(Icons.search),
                   suffixIcon: const Icon(Icons.tune),
                   filled: true,
-                  fillColor: Colors.white,
+                  fillColor: Colors.grey.shade100,
                   contentPadding: const EdgeInsets.symmetric(vertical: 0),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(19),
@@ -59,8 +58,32 @@ class Home extends StatelessWidget {
                   ),
                 ),
               ),
+              const SizedBox(height: 16),
+              //vehicle Type FIlter Chips
+              Row(
+                children: [
+                  _buildFilterChip("All", selected: true),
+                  _buildFilterChip("Car"),
+                  _buildFilterChip("Truck"),
+                  _buildFilterChip("Micro"),
+                ],
+              ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFilterChip(String label, {bool selected = false}) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 8.0),
+      child: Chip(
+        label: Text(label),
+        labelStyle: TextStyle(color: selected ? Colors.white : Colors.black),
+        backgroundColor: selected ? Colors.blue : Colors.grey.shade200,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusGeometry.circular(10),
         ),
       ),
     );
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme() {
-  return ThemeData(
-    fontFamily: "Cabin",
-    scaffoldBackgroundColor: const Color(0xFFF3F5F7),
-  );
+  return ThemeData(fontFamily: "Cabin", scaffoldBackgroundColor: Colors.white);
 }
